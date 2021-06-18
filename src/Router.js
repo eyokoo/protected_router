707e4348f24411748aadf02b019805c08bade0a3
Emi Yokoo Mantilla <emimantilla@Christians-Air.lan1>
2021-06-17 19:51:44 -0500

    finished

--- src/Router.js
+++ src/Router.js
@@ -5,21 +5,37 @@ import Home from './components/Home'
 import About from './components/About'
 import Car from './components/Car'
 import Login from './components/Login'
+import { Component } from 'react'
+import { FormatAlignLeftSharp } from '@material-ui/icons'
 
 // Write checkAuth function here
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
 // Check the cookies for a cookie called "loggedIn"
 
 
 // Write ProtectedRoute function here
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route
+            render={(props) => checkAuth()
+                ? <Component {...props} />
+                : <Redirect to="/login" />
+            }
+        />
+    )
+}
 
 
 const Router = () => {
     return (
         <Switch>
             <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute path="/about" component={About} />
+            <ProtectedRoute path="/car/:id" component={Car} />
         </Switch>
     );
 };
